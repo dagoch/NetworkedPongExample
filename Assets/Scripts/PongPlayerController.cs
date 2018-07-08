7c2c65f88441d5cc6abd2c72f937606ac555b9bf
dagoch <dagoch@gmail.com>
2018-07-08 14:51:12 -0400

    each player adjusts camera to its pov

--- Assets/Scripts/PongPlayerController.cs
+++ Assets/Scripts/PongPlayerController.cs
@@ -6,6 +6,11 @@ using UnityEngine.Networking;
 public class PongPlayerController : NetworkBehaviour {
 
     public GameObject playerPaddle;
+    public float cameraHeight = 3.0f;
+    public float cameraDistance = 7.0f;
+    public float lookToDistanceDivisor = 3.0f;
+    //public Camera playerCamera;
+    public float dirFromCtr;
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +19,23 @@ public class PongPlayerController : NetworkBehaviour {
 	
     public override void OnStartLocalPlayer()
     {
+        if (playerPaddle == null) playerPaddle = gameObject;
         playerPaddle.GetComponent<Renderer>().material.color = Color.blue;
+
+        float lookToDistance = cameraDistance / lookToDistanceDivisor;
+
+        //playerCamera.gameObject.SetActive(true);
+        Vector3 camPosition = transform.position;
+        dirFromCtr = Mathf.Sign(camPosition.z);
+
+        camPosition.z = dirFromCtr * cameraDistance;
+        camPosition.x = 0;
+        camPosition.y = cameraHeight;
+        Quaternion camRotation = transform.rotation;
+        camRotation.SetLookRotation(new Vector3(0,0,dirFromCtr*lookToDistance) - camPosition);
+        Camera.main.transform.SetPositionAndRotation(camPosition, camRotation);
+
+
     }
 
 	// Update is called once per frame
@@ -24,7 +45,7 @@ public class PongPlayerController : NetworkBehaviour {
             return;
         }
 
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f * -dirFromCtr;
         if ( (x < 0 && playerPaddle.transform.position.x > -1.8f) ||
             (x > 0 && playerPaddle.transform.position.x < 1.8f) )
             playerPaddle.transform.Translate(x, 0, 0,Space.World);
