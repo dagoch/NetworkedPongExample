60856515d196b0f422cfbf74182a9361abdcabb5
dagoch <dagoch@gmail.com>
2018-07-08 11:36:16 -0400

    initial commit of project

---
+++ Assets/Scripts/PongPaddleController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PongPaddleController : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f;
+        if ( (x < 0 && transform.position.x > -1.8f) ||
+            (x > 0 && transform.position.x < 1.8f) )
+            transform.Translate(x, 0, 0);
+         
+            
+	}
+}
---
+++ Assets/Scripts/PongPlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PongPlayerController : NetworkBehaviour {
+
+    public GameObject playerPaddle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public override void OnStartLocalPlayer()
+    {
+        playerPaddle.GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f;
+        if ( (x < 0 && playerPaddle.transform.position.x > -1.8f) ||
+            (x > 0 && playerPaddle.transform.position.x < 1.8f) )
+            playerPaddle.transform.Translate(x, 0, 0,Space.World);
+         
+            
+	}
+}
