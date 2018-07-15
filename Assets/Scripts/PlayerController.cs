297e5288637ecf834db00837ca7ad122d7a66765
dagoch <dagoch@gmail.com>
2018-07-15 15:16:48 -0400

    New google cardboard-enabled scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,17 @@ public class PlayerController : NetworkBehaviour
 {
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
+    public Transform cameraTransform;
+    public Transform visorTransform;
 
     public override void OnStartLocalPlayer()
     {
         GetComponent<Renderer>().material.color = Color.blue;
+        cameraTransform = Camera.main.transform;
+        //Camera.main.transform.parent = transform;
+        visorTransform = transform.Find("Visor");
+        Camera.main.transform.position = visorTransform.position;
+        Debug.Log("start local player: visor position = " + visorTransform.position + " camera posn = " + Camera.main.transform.position);
     }
 
     void Update()
@@ -18,13 +25,19 @@ public class PlayerController : NetworkBehaviour
             return;
         }
 
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+
+        // rotate the player to match the camera's rotation (controlled by GoogleVR)
+        transform.rotation = cameraTransform.rotation;
+
+
+        // move the player according to input
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        transform.Translate(0, 0, z); 
 
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+        // move the camera to match the player's position
+        cameraTransform.position = visorTransform.position;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)  || Input.GetMouseButtonUp(0)) // || Input.GetMouseButton(0)
         {
             CmdFire();
         }
