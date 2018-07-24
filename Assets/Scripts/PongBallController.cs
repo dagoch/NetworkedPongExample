2428d911aee40bc6b00bd729aeb091391a69106e
david lobser <davidlobser@yahoo.com>
2018-07-24 17:51:01 -0400

    pong ball is working

--- Assets/Scripts/PongBallController.cs
+++ Assets/Scripts/PongBallController.cs
@@ -5,36 +5,74 @@ using UnityEngine;
 public class PongBallController : MonoBehaviour {
 
     public Vector2 bounds = Vector2.one;
+    Vector2 direction = Vector2.zero;
 
     public float speed;
 
-    Vector3 nextPosition;
-    Vector3 lastPosition;
+   // Vector3 nextPosition;
+    //Vector3 lastPosition;
 
     float counter;
+
+    public PongPlayerController[] paddles;
+
+    public int scoreA = 0;
+    public int scoreB = 0;
    
 	void Start () {
-        GenerateNextPosition(Random.value < .5f ? true : false);
-        lastPosition = this.transform.localPosition;
+        direction = new Vector2(0, 1);
+        paddles = FindObjectsOfType<PongPlayerController>();
+//        GenerateNextPosition(Random.value < .5f ? true : false);
+        //lastPosition = this.transform.localPosition;
 	}
 	
 	void Update () {
-        LerpToNextPosition();
+        MovePuck();
+        CheckDirection();
 	}
 
-    void LerpToNextPosition()
+    void MovePuck()
     {
+//        float dist = Vector3.Distance(lastPosition, nextPosition);
         counter += Time.deltaTime * speed;
-        this.transform.localPosition = Vector3.Lerp(lastPosition, nextPosition, counter);
-        if (counter > 1)
-        {
-            counter = 0;
-            GenerateNextPosition(this.transform.localPosition.z < 0 ? true : false);
-            lastPosition = this.transform.position;
-        }
+        this.transform.Translate(new Vector3(direction.x,0,direction.y)*speed );
+       
 
     }
-    void GenerateNextPosition(bool upDown){
-        nextPosition = upDown ? new Vector3(0, 0, bounds.y) : new Vector3(0, 0, -bounds.y);
+
+    void CheckDirection(){
+        if (this.transform.localPosition.x > bounds.x)
+            HitWall();
+        if (this.transform.localPosition.x < -bounds.x)
+            HitWall();
+        if (this.transform.localPosition.z > bounds.y)
+        {
+            HitPaddle();
+
+        }
+        if (this.transform.localPosition.z < -bounds.y)
+        {
+            HitPaddle();
+        }
+    }
+   
+    void HitWall(){
+        direction = new Vector2(direction.x * -1, direction.y);
+    }
+    void HitPaddle(){
+
+        int whichPaddle =
+            Vector3.Distance(this.transform.localPosition, paddles[0].transform.localPosition) >
+            Vector3.Distance(this.transform.localPosition, paddles[1].transform.localPosition) ?
+                   1 : 0;
+        
+        direction = new Vector2((this.transform.localPosition.x-paddles[whichPaddle].transform.localPosition.x)*.5f, direction.y * -1);
+        if (Vector3.Distance(this.transform.localPosition, paddles[whichPaddle].transform.localPosition) > 1.5f)
+        {
+            if (whichPaddle == 0) scoreA += 1; else scoreB += 1;
+            direction = new Vector2(0, direction.y);
+
+            //Destroy(this.gameObject);
+        }
     }
 }
